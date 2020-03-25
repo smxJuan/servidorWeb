c800fde4e0a4949c31f0de33ce6d0fdc85ff9e9c
smxJuan <57349084+smxJuan@users.noreply.github.com>
2020-03-25 09:31:18 -0700

    Add files via upload

--- web/script-seccio-frontendproductes.js
+++ web/script-seccio-frontendproductes.js
@@ -20,17 +20,44 @@ class ObjSeccioFrontendProductes {
         objRebut = await promiseCallServer('POST', '/call/llistatProductes', {})
 
         // Transformem l'objecte rebut en codi HTML
-        if (objRebut.resultat === 'ok') {
-            for (cntProducte = 0; cntProducte < objRebut.missatge.length; cntProducte = cntProducte + 1) {
-                valor = objRebut.missatge[cntProducte]
-                codiHTML = codiHTML + '<img src="' + valor.imatge + '" width="100" />'
-                codiHTML = codiHTML + '<h3>' + valor.nom +'</h3>'
-                codiHTML = codiHTML + '<div>' + valor.descripcio +'</div>'
-                codiHTML = codiHTML + '<div>' + valor.preu +' €</div>'
-                codiHTML = codiHTML + '<hr/>'
-            }
-        }
+        codiHTML = codiHTML + '<div class="primera-seccion-productos">'
+            codiHTML = codiHTML + '<div class="imagen-oferta-dia">'
+                codiHTML = codiHTML + '<img src="/web/imatges/oferta-madrid.jpeg" width="100%" height="400px"/>'
+            codiHTML = codiHTML + '</div>'
+            codiHTML = codiHTML + '<div class="texto-oferta-dia">'
+                codiHTML = codiHTML + '<div class="texto-oferta-dia-izq">'
+                    codiHTML = codiHTML + '<h1>¡Oferta del dia!</h1>'
+                codiHTML = codiHTML + '</div>'
+                codiHTML = codiHTML + '<div class="texto-oferta-dia-der">'
+                    codiHTML = codiHTML + '<h3>Conocer Madrid por solo 50€</h3>'
+                    codiHTML = codiHTML +  '<p>¡OFERTA DE LA SEMANA! Aprovecha esta gran oportunidad, de conocer la capital de España, por tan solo 50€, no te lo pierdas!!<p>'
+                    codiHTML = codiHTML + '<div class="boton-compra-dia">'
+                            codiHTML = codiHTML + '<div class="letra-boton-compra">Comprar</div>'
+                        codiHTML = codiHTML + '</div>'
+                codiHTML = codiHTML + '</div>'
+            codiHTML = codiHTML + '</div>'
+        codiHTML = codiHTML + '</div>'
 
+        codiHTML = codiHTML + '<div class="segunda-seccion-productos">'
+            codiHTML = codiHTML + '<h1>Ofertas del Mes</h1>'
+            codiHTML = codiHTML + '<div class="productos-oferta-del-mes">'
+            if (objRebut.resultat === 'ok') {  
+                for (cntProducte = 0; cntProducte < objRebut.missatge.length; cntProducte = cntProducte + 1) {
+                    valor = objRebut.missatge[cntProducte]
+                    codiHTML = codiHTML + '<div class="boton-oferta">'
+                        codiHTML = codiHTML + '<img src="' + valor.imatge + '" width="100%" + height="50%" />'
+                        codiHTML = codiHTML + '<h3>' + valor.nom +'</h3>'
+                        codiHTML = codiHTML + '<div>' + valor.descripcio +'</div>'
+                        codiHTML = codiHTML + '<div>' + valor.preu +' € tarifa mediana</div>'
+                        codiHTML = codiHTML + '<div class="boton-compra">'
+                            codiHTML = codiHTML + '<div class="letra-boton-compra" onclick="navegacio.canviaSeccio("frontendHome")">Comprar</div>'
+                        codiHTML = codiHTML + '</div>'
+                    codiHTML = codiHTML + '</div>'
+                }
+            }
+        codiHTML = codiHTML + '</div>'
+        codiHTML = codiHTML + '</div>'
+        
         // Amaguem la càrrega i mostrem el llistat de productes
         refContinguts.innerHTML = codiHTML
         refContinguts.style.display = 'flex'
